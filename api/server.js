e4b5ebdec04d273b5e7c2128fd414e85668fc376
Ethan Edmond <eedmond@uccs.edu>
2021-07-15 14:20:20 -0600

    Added db-config and imported it to server

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const server = express();
 
+const db = require('../data/db-config');
+
+
 server.use(express.json());
 
 server.use('/api/recipe', (req, res, next) => {
-
+  // 
 });
 
 server.use((err, req, res, next) => {
