5136d699c84e342753f7694ac53b035b502889a4
Ethan Edmond <eedmond@uccs.edu>
2021-07-15 17:31:37 -0600

    Finished query, working on js

--- api/server.js
+++ api/server.js
@@ -7,11 +7,48 @@ const db = require('../data/db-config');
 
 server.use(express.json());
 
-server.get('/api/recipes', async (req, res, next) => {
-  await db('recipes');
-  res.json({
-    message: "it's working"
-  });
+server.get('/api/recipes/:id', async (req, res, next) => {
+  // SELECT
+  // recipe_name, created_at, step_number, instructions, ingredient_name, quantity
+  // FROM recipes
+  // LEFT JOIN steps
+  // ON recipes.recipe_id = steps.recipe_id
+  // LEFT JOIN step_ingredients
+  // ON steps.step_id = step_ingredients.step_id
+  // LEFT JOIN ingredients
+  // ON step_ingredients.ingredient_id = ingredients.ingredient_id
+  // WHERE recipes.recipe_id = 2
+  // ORDER BY step_number
+  try {
+    const queryRes = await db('recipes')
+          .select(
+            'recipes.*', 'steps.*', 'ingredient_name', 'quantity'
+          )
+          .leftJoin('steps', 'recipes.recipe_id', 'steps.recipe_id')
+          .leftJoin('step_ingredients', 'steps.step_id', 'step_ingredients.step_id')
+          .leftJoin('ingredients', 'step_ingredients.ingredient_id', 'ingredients.ingredient_id')
+          .where({'recipes.recipe_id': req.params.id})
+          .orderBy('step_number');
+    const ripRecipe = queryRes.reduce((acc, curr) => {
+      const currStep = null;
+      return {
+        ...acc,
+        recipe_id: curr.recipe_id,
+        recipe_name: curr.recipe_name,
+        created_at: curr.created_at,
+        steps: [...acc.steps, currStep]
+      };
+    }, {
+      recipe_id: null,
+      recipe_name: null,
+      created_at: null,
+      steps: []
+    });
+
+    res.json(ripRecipe);
+  } catch (err) {
+    next(err);
+  }
 });
 
 server.use((err, req, res, next) => {
--- data/seeds/04-step_ingredients.js
+++ data/seeds/04-step_ingredients.js
@@ -1,13 +1,21 @@
+const stiMaker = (step_id, ingredient_id, quantity) => {
+  return {step_id, ingredient_id, quantity};
+};
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('step_ingredients').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('step_ingredients').insert([
+        stiMaker(1,1,1),
+        stiMaker(1,2,1),
+        stiMaker(3,7,1),
+        stiMaker(3,8,1),
+        stiMaker(5,5,1),
+        stiMaker(5,6,1),
+        stiMaker(7,3,1),
+        stiMaker(7,4,1)
       ]);
     });
 };
